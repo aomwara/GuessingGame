97e33c70d3b62d9882349fe49529e5f0ad454a65
waranat suttikarn <waranat.cpe@gmail.com>
2021-02-15 19:25:01 +0700

    install backend

---
+++ backend/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    
+    "github.com/gorilla/mux"
+)
+
+func main() {
+    r := mux.NewRouter()
+
+    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "<h1>This is the homepage. Try /hello and /hello/Sammy\n</h1>")
+    })
+
+    r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "<h1>Hello from Docker!\n</h1>")
+    })
+
+    r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["name"]
+
+        fmt.Fprintf(w, "<h1>Hello, %s!\n</h1>", title)
+    })
+
+    http.ListenAndServe(":80", r)
+}
